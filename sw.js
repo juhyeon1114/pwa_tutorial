4fbb3b29f61ee55ab883453de7f57de081085cda
juhyeon1114 <juhyeon1114@gmail.com>
2020-08-25 22:38:48 +0900

    detail

--- sw.js
+++ sw.js
@@ -1,10 +1,3 @@
-/************************************************
- * 서비스 워커 설치
- * -> 서비스 워커가 브라우저에 등록된 후, 캐싱을 설치하는 것
- * -> CACHE_NAME : 캐싱 스토리지에 저장될 파일 이름
- * -> filesToCache : 캐싱할 웹 자원(css, 이미지등등)의 목록
- * -> self === window객체
- ************************************************/
 var CACHE_NAME = 'pwa-offline-v1';
 var filesToCache = [
     '/',
@@ -14,6 +7,13 @@ var filesToCache = [
     '/images/icons/512icon.png'
 ];
 
+/************************************************
+ * 서비스 워커 설치
+ * -> 서비스 워커가 브라우저에 등록된 후, 캐싱을 설치하는 것
+ * -> CACHE_NAME : 캐싱 스토리지에 저장될 파일 이름
+ * -> filesToCache : 캐싱할 웹 자원(css, 이미지등등)의 목록
+ * -> self === window객체
+ ************************************************/
 self.addEventListener('install', function(event){
     // 캐시등록 또는 기타 로직 수행
     console.log('[Service Worker] Install');
@@ -49,3 +49,32 @@ self.addEventListener('fetch', function(event){
             })
     );
 });
+
+/************************************************
+ * 서비스 워커 활성화 및 업데이트
+ * -> 새로운 서비스워커가 설치되면 활성화 단계로 넘어온다.
+ * -> 이전에 사용하던 서비스워커와 캐시를 비운다
+ * -> caches.keys() : 기존에 사용하던 캐시 목록 확인
+ ************************************************/
+self.addEventListener('activate', function(event){
+    console.log('[Service Worker] Activate');
+    var newCacheList = ['pwa-offline-v2'];
+    event.waitUntil(
+        caches.keys().then(function(cacheList){
+            return Promise.all(
+                cacheList.map(function(cacheName){
+                    if (newCacheList.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).catch(function(error){
+            return console.error(error);
+        })
+    );
+});
+
+/************************************************
+ * 서비스 워커 라이프 사이클
+ * -> 서비스워커는 웹페이지와 별개의 생명주기를 갖는다  
+ ************************************************/
